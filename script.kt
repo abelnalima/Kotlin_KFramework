bd92d9b5ace96dbf113a60e8a4250eadf5e8f6ac
abelnalima <abel.na.lima@gmail.com>
2020-06-16 23:13:32 -0300

    Declaracao passando valor. Expressoes aritmeticas e booleanas.

--- script.kt
+++ script.kt
@@ -1,5 +1,14 @@
 fun main() {
 
-    var x String
+    //var x1 Int = 10 * 10
+    //val x2 Int = 30 / 15
+    //var y1 Boolean = !(5<2)
+    //val y2 Boolean = 25 >= 20
+    //var z1 String
+    //val z2 String
+
+    var x Int = 10
+    x--
+    x --
 
 }
