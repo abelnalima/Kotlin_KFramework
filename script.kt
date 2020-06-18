19dcbee28b732fa0cd4ebc550fc9e44485c78bdb
abelnalima <abel.na.lima@gmail.com>
2020-06-18 14:32:00 -0300

    Inclusao do statement IF, ElSEIF e ELSE

--- script.kt
+++ script.kt
@@ -1,14 +1,9 @@
 fun main() {
 
-    //var x1 Int = 10 * 10
-    //val x2 Int = 30 / 15
-    //var y1 Boolean = !(5<2)
-    //val y2 Boolean = 25 >= 20
-    //var z1 String
-    //val z2 String
+    var x Int = 5
 
-    var x Int = 10
-    x--
-    x --
+    if(5 > 2 && 5 > 10){
+        x = 10
+    }
 
 }
